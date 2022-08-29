4650196f0913868cb4d4b04d66f8ac60b6852b98
Dat <s3819827@rmit.edu.vn>
2022-08-29 18:53:00 +0700

    save XO button

---
+++ Helper/AVfile.swift
@@ -0,0 +1,20 @@
+//
+//  AVfile.swift
+//  XO_games
+//
+//  Created by dat on 29/08/2022.
+//
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+  if let path = Bundle.main.path(forResource: sound, ofType: type) {
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+      audioPlayer?.play()
+    } catch {
+      print("ERROR: Could not find and play the sound file!")
+    }
+  }
+}
---
+++ XO_games/XOButton.swift
@@ -0,0 +1,43 @@
+//
+//  XOButton.swift
+//  XO_games
+//
+//  Created by dat on 29/08/2022.
+//
+
+import SwiftUI
+
+struct XOButton: View {
+    @Binding var character: String
+    @State private var point = 0.0
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 120, height: 120)
+                .foregroundColor(.gray)
+            Circle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.white)
+            Text(character)
+                .font(.system(size: 50))
+                .bold()
+        }
+        .rotation3DEffect(.degrees(point), axis: (x: 1, y: 0, z: 0))
+        .simultaneousGesture(
+            TapGesture()
+                .onEnded { _ in
+                    withAnimation(.easeIn(duration: 0.25)) {
+                        self.point -= 180
+                    }
+                }
+        )
+    }
+}
+
+
+struct XOButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XOButton(character: .constant("X"))
+    }
+}
