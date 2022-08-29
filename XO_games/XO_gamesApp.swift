c3ff65fdae62b96fbf0717a22309b95c0bbf38eb
Dat <s3819827@rmit.edu.vn>
2022-08-29 13:52:09 +0700

    Initial Commit

---
+++ XO_games/XO_gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  XO_gamesApp.swift
+//  XO_games
+//
+//  Created by dat on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct XO_gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
